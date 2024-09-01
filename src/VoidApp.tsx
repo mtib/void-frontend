8acf891058ad9ad9c581a73a1c220e0f848b5579
Markus Becker <mtib.becker@gmail.com>
2024-09-01 22:15:46 +0200

    feat: communicate with server

--- src/VoidApp.tsx
+++ src/VoidApp.tsx
@@ -1,12 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './VoidApp.css';
+import { SERVER_URL } from './server';
 
 function VoidApp() {
   const [count, setCount] = useState(0);
 
+  const [message, setMessage] = useState('VOID');
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(`${SERVER_URL}/`);
+      const responseData = await response.json() as { message: string; };
+      setMessage(responseData.message);
+    })();
+  }, []);
+
   return (
     <>
-      <h1>Void</h1>
+      <h1>{message}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/server.ts
@@ -0,0 +1,1 @@
+export const SERVER_URL = import.meta.env.MODE === 'development' ? 'http://localhost:49420' : 'https://api.void.mtib.dev';
