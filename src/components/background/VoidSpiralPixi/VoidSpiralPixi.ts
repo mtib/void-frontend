cd0d2417b53e7f61a5830dbfd084b2fb640e6a94
Markus Becker <mtib.becker@gmail.com>
2024-09-03 14:43:33 +0200

    feat: less flickery boot

--- src/components/background/VoidSpiralPixi/VoidSpiralPixi.ts
+++ src/components/background/VoidSpiralPixi/VoidSpiralPixi.ts
@@ -2,6 +2,7 @@ import { Application, Container, Graphics, GraphicsContext, Point } from "pixi.j
 import { dot, magnitude, normalize, outsideRectSample } from "./utils";
 import { VoidContextType } from "../../../context/VoidContext";
 import { DocumentContextType } from "../../../context/DocumentContext";
+import _ from "lodash";
 
 export function install(
     app: Application,
@@ -32,15 +33,16 @@ export function install(
         const g = new Graphics();
         g.circle(0, 0, blackHoleSize);
         g.fill({
-            color: '#000',
+            color: '#150215',
             alpha: 1.0
         });
         g.stroke({
-            color: '#fff',
+            color: '#dad',
             width: 1.5,
         });
         g.x = app.canvas.width / 2;
         g.y = app.canvas.height / 2;
+        g.scale = 0;
         return g;
     })();
 
@@ -59,36 +61,53 @@ export function install(
         .regularPoly(0, 0, starSize, 6)
         .fill({ color: '#dad' });
 
+    const starSizeFactor = new WeakMap<Graphics, number>();
     for (let i = 0; i < starCount; i++) {
         const star = new Graphics(starContext);
         star.x = (Math.random() * 2 - 0.5) * app.canvas.width;
         star.y = (Math.random() * 2 - 0.5) * app.canvas.height;
         star.rotation = Math.random() * Math.PI * 2;
-        const size = Math.max(1, 0.5 + Math.random());
-        star.scale.set(size, size);
+        starSizeFactor.set(star, Math.max(1, 0.5 + Math.random()));
+        star.scale.set(0, 0);
         starContainer.addChild(star);
     }
 
-    let elapsed = 0;
+    let elapsedDt = 0;
+    let elapsedMs = 0;
     app.ticker.add(() => {
-        elapsed += app.ticker.deltaTime;
+        elapsedDt += app.ticker.deltaTime;
+        elapsedMs += app.ticker.elapsedMS;
 
         const maxTravel = Math.min(minEdge / 5, 100);
         const centerOfGravity = new Point(
-            app.canvas.width / 2 + Math.sin(randomPhaseX + elapsed / 1000) * maxTravel,
-            app.canvas.height / 2 + Math.cos(randomPhaseY + elapsed / 700 + 1) * maxTravel,
+            app.canvas.width / 2 + Math.sin(randomPhaseX + elapsedDt / 1000) * maxTravel,
+            app.canvas.height / 2 + Math.cos(randomPhaseY + elapsedDt / 700 + 1) * maxTravel,
         );
         blackHole.x = centerOfGravity.x;
         blackHole.y = centerOfGravity.y;
 
+        if (blackHole.scale.x < 1) {
+            blackHole.scale = Math.min(Math.max(0, Math.exp(elapsedMs / 300 - 0.3) - 1), 1);
+        }
+
         starContainer.children.forEach((star) => {
+            if (!(star instanceof Graphics)) {
+                return;
+            }
+
             const diff = new Point(
                 star.x - centerOfGravity.x,
                 star.y - centerOfGravity.y
             );
 
+            const sizeFactor = starSizeFactor.get(star) || 1;
+            if (star.scale.x < sizeFactor) {
+                const scale = Math.min(sizeFactor, sizeFactor * elapsedMs / 500.0);
+                star.scale = scale;
+            }
+
             const distance = magnitude(diff);
-            if (distance < blackHoleSize * 0.9) {
+            if (distance < blackHoleSize * blackHole.scale.x - starSize) {
                 const outSide = outsideRectSample();
                 const inOutSide = Math.random() * 2 - 0.5;
                 if (Math.random() < 0.5) {
@@ -103,8 +122,8 @@ export function install(
 
             const upDotProductCenter = dot(normalize(diff), new Point(0, 1));
             const leftDotProductCenter = dot(normalize(diff), new Point(-1, 0));
-            star.x -= diff.x / distance ** 2 + upDotProductCenter * swirlStrength * ((minEdge - distance) / minEdge) ** 5;
-            star.y -= diff.y / distance ** 2 + leftDotProductCenter * swirlStrength * ((minEdge - distance) / minEdge) ** 5;
+            star.x -= (diff.x / (distance / 10) ** 2 + upDotProductCenter * swirlStrength * ((minEdge - distance) / minEdge) ** 5) * blackHole.scale.x;
+            star.y -= (diff.y / (distance / 10) ** 2 + leftDotProductCenter * swirlStrength * ((minEdge - distance) / minEdge) ** 5) * blackHole.scale.x;
             star.rotation += app.ticker.deltaTime;
         });
     });
